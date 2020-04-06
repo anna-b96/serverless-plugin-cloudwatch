a289b3ac3bc1a4bae0fc12908fab99ea95a4a33b
anna-b96 <anna.blendl@codecentric.de>
2020-04-06 10:27:15 +0200

    added some tests

--- __tests__/DashboardPluginTest.js
+++ __tests__/DashboardPluginTest.js
@@ -72,6 +72,72 @@ test('add dashboard to existing resources when not missing', t => {
         }
     })
 })
+test('adding dashboard integration test', t => {
+    const serverless = {
+        cli: {
+            log: msg => {}
+        },
+        service: {
+            service: 'project-name',
+            custom: {
+                dashboard: {
+                    lambda: {
+                        widgets: [
+                            { name: 'Sum of Invocations',
+                                metrics: [
+                                    { name: 'Invocations', stat: 'Sum' },
+                                ]},
+                            { name: 'Sum of Errors',
+                                metrics: [
+                                    { name: 'Errors', stat: 'Sum'}
+                                ]
+
+                            }],
+                        enabled: true
+                    }
+                }
+            },
+            functions: {
+                'function1': {},
+                'function2': {}
+            },
+            provider: {
+                stage: 'dev',
+                region: 'eu-central-1',
+                compiledCloudFormationTemplate: {
+                    Resources: {
+                        otherResource: 'dont touch me'
+                    }
+                }
+            }
+        }
+    }
+    const dashboardPlugin = new DashboardPlugin(serverless, {})
+    //sinon.stub(dashboardPlugin, 'createDashboard').returns(dummyDashboard)
+    dashboardPlugin.addDashboard()
+
+    t.deepEqual(serverless.service.provider.compiledCloudFormationTemplate, {
+        Resources: {
+            otherResource: 'dont touch me',
+            'ProjectOverviewDashboard': {
+                Type: 'AWS::CloudWatch::Dashboard',
+                Properties: {
+                    DashboardName: 'project-name-dev',
+                    DashboardBody: '{"widgets":' +
+                        '[{"type":"metric","width":24,"height":6,"properties":{"region":"eu-central-1","title":"Sum of Errors","metrics":' +
+                            '[["AWS/Lambda","Errors","FunctionName","function1",{"stat":"Sum"}],' +
+                            '["AWS/Lambda","Errors","FunctionName","function2",{"stat":"Sum"}]],' +
+                        '"stat":"Sum","view":"timeSeries","stacked":false,"period":60}},' +
+
+                        '{"type":"metric","width":24,"height":6,"properties":{"region":"eu-central-1","title":"Sum of Invocations","metrics":' +
+                            '[["AWS/Lambda","Invocations","FunctionName","function1",{"stat":"Sum"}],' +
+                            '["AWS/Lambda","Invocations","FunctionName","function2",{"stat":"Sum"}]],' +
+                        '"stat":"Sum","view":"timeSeries","stacked":false,"period":60}}]}'
+                }
+            }
+        }
+    })
+})
 // ---------------------------------- tests for createDashboard() ---------------------------------- //
 test('create dashboard', t => {
     const serverless = {
@@ -99,8 +165,8 @@ test('create dashboard', t => {
                 }
             },
             functions: {
-                's-dev-f1': {},
-                's-dev-f2': {}
+                'function1': {},
+                'function2': {}
             },
             provider: {
                 stage: 'dev',
@@ -116,11 +182,10 @@ test('create dashboard', t => {
 
     const dashboardPlugin = new DashboardPlugin(serverless, {})
     const dashboard = dashboardPlugin.createDashboard()
-
     t.is(dashboard.Type, 'AWS::CloudWatch::Dashboard')
     t.deepEqual(JSON.parse(dashboard.Properties.DashboardBody).widgets.length, 2)
     t.deepEqual(dashboard.Properties.DashboardName, 'project-name-dev')
-    t.deepEqual(JSON.parse(dashboard.Properties.DashboardBody).widgets[0].properties.metrics, [['AWS/Lambda', 'Errors', 'FunctionName', 's-dev-f1', { 'stat': 'Sum' }],['AWS/Lambda', 'Errors', 'FunctionName', 's-dev-f2', { 'stat': 'Sum' }]])
+    t.deepEqual(JSON.parse(dashboard.Properties.DashboardBody).widgets[0].properties.metrics[0][3], 'function1')
    //t.deepEqual(dashboard, '')
 })
 
